738349a5eebd2520bbb4e11f23e6a8bc7e43f6a6
Hyeonseok Kim <kim010322@gmail.com>
2020-04-29 00:35:54 +0900

    Refactoring

--- main.c
+++ main.c
@@ -1,86 +1,64 @@
 #include <stdio.h>
-#define SIZE 100	//size of cells
+#include <stdbool.h>
+#define CELL_SIZE 110
 
-//Array for Cells
-int arr[SIZE] = { 0 };
-int temp[SIZE];
+typedef struct _Cell
+{
+	bool current;
+	bool next;
+} Cell;
 
-//Define functions
-void setup();
-void print();
+Cell cell[CELL_SIZE] = { 0, };
+
+void render();
 void process();
-void transfer();
+void update();
 
 int main()
 {
-	char buffer;
-	int gen = 1;
-	setup();
-	while (1)
+	cell[CELL_SIZE / 2].current = true;
+	for (int i = 0; i < 100; i++)
 	{
-		print();	//print arr
-		process();	//temp =arr
-		transfer();	//temp-> arr
-		printf("%6d", gen);
-		buffer = getchar();
-		gen++;
+		render();
+		process();
+		update();
 	}
 	return 0;
 }
 
-void setup()
-{
-	arr[SIZE / 2] = 1;
-}
-
-void print()
+void render()
 {
 	int i;
-	for (i = 0; i < SIZE; i++)
+	for (i = 0; i < CELL_SIZE; i++)
 	{
-		if (arr[i] == 1)	//alive
+		if (cell[i].current)
 		{
-			printf("O");
+			printf("бс");
 		}
-		else if (arr[i] == 0)	//dead
+		else
 		{
-			printf("X");
+			printf("  ");
 		}
 	}
+	printf("\n");
 }
 
 void process()
 {
-	int n;
-	for (n = 0; n < SIZE; n++)
+	int i;
+	cell[0].next = false;
+	cell[CELL_SIZE - 1].next = false;
+	for (i = 1; i < CELL_SIZE-1; i++)
 	{
-		if (arr[n] == 0)
-		{
-			temp[n] = (arr[n - 1] ^ arr[n + 1]);
-		}
-		else if (arr[n] == 1)
-		{
-			if (arr[n - 1] + arr[n + 1] == 0)
-			{
-				temp[n] = 1;
-			}
-			else if (arr[n - 1] == 0 && arr[n + 1] == 1)
-			{
-				temp[n] = 1;
-			}
-			else
-			{
-				temp[n] = 0;
-			}
-		}
+		cell[i].next = cell[i - 1].current ^ (cell[i].current || cell[i + 1].current);
 	}
 }
 
-void transfer()
+void update()
 {
 	int i;
-	for (i = 0; i < SIZE; i++)
+	for (i = 0; i < CELL_SIZE; i++)
 	{
-		arr[i] = temp[i];
+		cell[i].current = cell[i].next;
 	}
 }
